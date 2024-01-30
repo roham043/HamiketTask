98e9acbf32fa4187808807e274103c05bc4ff7b4
roham043 <66734575+roham043@users.noreply.github.com>
2024-01-30 22:06:31 +0330

    install and create modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Form from './Component/Form';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
   return (
     <div className="App">
-     <Form/>
-       
+
+      <button onClick={onOpenModal}>Open modal</button>
+      <Modal open={open} onClose={onCloseModal} center>
+        <Form />
+      </Modal>
+
     </div>
   );
 }
