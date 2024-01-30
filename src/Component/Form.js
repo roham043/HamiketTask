d66725bf2410dfec6297c3a4f7b92f5c88a2a8f0
roham043 <66734575+roham043@users.noreply.github.com>
2024-01-30 17:12:39 +0330

    use yup for register form component

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,29 +1,54 @@
+import { useForm } from 'react-hook-form';
 import { DtPicker } from 'react-calendar-datetime-picker'
-import 'react-calendar-datetime-picker/dist/style.css'
+import 'react-calendar-datetime-picker/dist/style.css';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup'
 
 const Form = () => {
+    const schema = yup.object().shape({
+        fullname: yup.string().required('این فیلد نباید خالی باشد'),
+        age:yup.number().positive().integer().min(18,'شما اجازه دسترسی به سایت را ندارید').required(),
+      
+        
+    })
+
+    const { register, handleSubmit ,formState:{errors} } = useForm({
+        resolver:yupResolver(schema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
 
-        <form>
-            <input type="text" placeholder=" نام و نام خانوادگی" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder=" نام و نام خانوادگی" {...register('fullname')} />
+            <p>{errors.fullname?.message}</p>
             <p>جنسیت:</p>
-            <input type="radio" id="male" name="male" />
+            <input type="radio" id="male" name="male" {...register('male')} />
             <label for="html">مرد</label><br />
-            <input type="radio" id="femail" name="femail" />
+            <input type="radio" id="femail" name="femail" {...register('femail')} />
             <label for="css">زن</label><br />
 
-            <DtPicker local='fa' yearListStyle='list' />
-            <label for="age">بازه سنی:</label>
-            <select name="age" id="age">
-                <option value="0-18">0-18</option>
-                <option value="19-30">19-30</option>
-                <option value="31-45">31-45</option>
-                <option value="46">46</option>
-            </select>
+            {/* <DtPicker onChange={true} local='fa' yearListStyle='list' {...register('brtDayfa')} /> */}
             <br/>
-            <label for="img">عکس پروفایل:</label>
-            <input type="file" id="img" name="img" accept="image/*" placeholder='gggg'  />
-
+            <label for="birthday">Birthday:</label>
+            <input type="date" id="birthday" name="birthday" local='fa' {...register('brtDay')}/>
+            <br/>
+                <label for="age">بازه سنی:</label>
+                <input type='number' {...register('age')}/>
+                <p>{errors.age?.message}</p>
+                {/* <select name="age" id="age" {...register('age')}>
+                    <option value="0-18" >0-18</option>
+                    <option value="19-30" >19-30</option>
+                    <option value="31-45" >31-45</option>
+                    <option value="46" >46</option>
+                </select> */}
+                <br />
+                <label for="img">عکس پروفایل:</label>
+                <input type="file" id="img" name="img" accept="image/*" placeholder='gggg'  {...register('image')} />
+                <br />
+                <input type='submit' />
 
         </form>
 
